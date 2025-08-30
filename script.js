2bb2de3e1ce9f4ea629fa33f0cf312656af9fab2
Vasuki t <iamvasukithangaraj@gmail.com>
2025-08-30 12:19:54 +0530

    added errors in signUp form

--- script.js
+++ script.js
@@ -1,19 +1,62 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-// Redirect to signup.html when clicking the link
-    document.getElementById("signUpLink").addEventListener("click", function(e) {
-      e.preventDefault();
-      window.location.href = "signup.html";
+    // Redirect to signup.html when clicking the link
+    const signUp = document.getElementById("signUpLink");
+    signUp.addEventListener("click", function (e) {
+        e.preventDefault();
+        window.location.href = "signup.html";
     });
+ })
+    // for getting us into signup form
+    document.addEventListener("DOMContentLoaded", () => {
+       const signUpForm = document.getElementById("signupForm");
 
+        signUpForm.addEventListener("submit", function (e) {
+        e.preventDefault();
 
-document.getElementById("signupForm").addEventListener("submit", function(e) {
-      e.preventDefault();
-      alert("Signup successful! Redirecting to Home...");
-      setTimeout(() => {
-        window.location.href = "index.html"; // Go back to main page
-      }, 2000);
+        let validationSuccess = true;
+
+        // regex pattern 
+        const namePattern = /^[A-Za-z]+$/;
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const passwordPattern = /^[A-Za-z0-9!@#$%^&*/]{8,}$/;
+
+        // Input fields
+        const username = document.getElementById("userName");
+        const email = document.getElementById("Email");
+        const password = document.getElementById("password");
+
+        // clear previous errors
+        document.querySelectorAll(".error").forEach(clear => clear.textContent = "");
+
+        // testing username 
+        if(!namePattern.test(username.value)) {
+            document.getElementById("usernameError").innerText = "Username must contain letters";
+            validationSuccess = false;
+        }
+
+        //testing email
+        if (!emailPattern.test(email.value)) {
+            document.getElementById("EmailError").innerText = "Enter the valid Email";
+            validationSuccess = false;
+        }
+
+        // testing password
+        if (!passwordPattern.test(password.value)) {
+            document.getElementById("PasswordError").innerText = "Password must contain 8 charaters";
+            validationSuccess = false;
+        }
+
+        if (validationSuccess) {
+            alert("Signup successful! Redirecting to Home...");
+            signUpForm.reset();
+             setTimeout(() => {
+            window.location.href = "index.html"; // Go back to main page
+        }, 2000);
+        }
+
+       
     });
 
 })
